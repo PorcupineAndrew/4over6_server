411419d16b1e467499e20a2dac19c8e93c28c15b
陈熠豪 <chenyiha17@mails.tsinghua.edu.cn>
2020-03-17 15:02:11 +0800

    update with User Info

--- src/Macro.h
+++ src/Macro.h
@@ -8,10 +8,24 @@
 #include <errno.h>
 #include <string.h>
 #include <net/if.h>
+#include <sys/epoll.h>
+#include <fcntl.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <pthread.h>
 
-#define N_USERS 128 // 地址池大小
-#define IPADDR(a, b, c, d) (((a) << 24) | ((b) << 16) | ((c) << 8) | (d)) // 地址转换
-#define POOL_START_ADDR IPADDR(13, 8, 0, 2) // 地址池起始地址
+#ifndef __cplusplus
+typedef unsigned char bool;
+#endif
+
+// 地址相关
+#define N_USERS 128                                                         // 地址池大小
+#define IPADDR(a, b, c, d) (((a) << 24) | ((b) << 16) | ((c) << 8) | (d))   // 地址转换
+#define POOL_START_ADDR IPADDR(13, 8, 0, 2)                                 // 地址池起始地址
+
+// 客户信息
+#define ASSIGNED            1
+#define FREE                0
 
 // 消息类型
 #define IP_REQUEST          100
@@ -20,7 +34,11 @@
 #define NETWORK_RESPONSE    103
 #define KEEPALIVE           104
 
+// 服务器相关
 #define SERVER_LISTEN_PORT  10086
 #define MAX_LISTEN_QUEUE    16
+
+// epoll相关
+#define MAXEVENTS           64
 
 #endif
--- src/User_Info.c
+++ src/User_Info.c
@@ -1,8 +1,41 @@
 #include "User_Info_Table.h"
 
-void init_user_info(uint32_t start_addr) {
-    for(int i = 0; i < N_USERS; i++) {
+void init_user_info(uint32_t start_addr, int n_users) {
+    for(int i = 0; i < n_users; i++) {
         user_info_table[i].fd = -1;
         user_info_table[i].v4addr.s_addr = htonl(start_addr+i); // 网络字节序
+        user_info_table[i].count = FREE;
     }
+
+    fprintf(stdout, 
+        "User info init\n\tstart_addr: 0x%08x\n\tmax_users: %d\n",
+        start_addr, n_users
+    );
+}
+
+int insert_user_table(int infd, struct in6_addr *inaddr, int n_users, pthread_mutex_t *mutex) {
+	pthread_mutex_lock(mutex);
+    int i = 0;
+    for(; i < n_users; i++) {
+        if (user_info_table[i].count == FREE) {
+            user_info_table[i].count = ASSIGNED;
+            user_info_table[i].fd = infd;
+            user_info_table[i].secs = time(NULL);
+            memcpy(&user_info_table[i].v6addr, inaddr, sizeof(struct in6_addr));
+            break;
+        }
+    }
+	pthread_mutex_unlock(mutex);
+
+    if (i == n_users) return -1;
+
+    char s4[24], s6[64];
+    inet_ntop(AF_INET, &user_info_table[i].v4addr.s_addr, s4, 24);
+    inet_ntop(AF_INET6, inaddr, s6, 64);
+    fprintf(stdout, 
+        "User info update\n\tindex: %d\n\tv4_addr: %s\n\tv6_addr: %s\n",
+        i, s4, s6
+    );
+
+    return 0;
 }
