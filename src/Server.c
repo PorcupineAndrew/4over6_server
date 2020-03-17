36ea4384a5425ccfdbdbee96412ea142de25bdaa
陈熠豪 <chenyiha17@mails.tsinghua.edu.cn>
2020-03-17 15:02:53 +0800

    update with error handling

--- src/Server.c
+++ src/Server.c
@@ -6,7 +6,7 @@ extern int listen_fd;
 void init_server(int port, char* addr, int cons, char* if_name) {
     // 创建套接字
     if ((listen_fd = socket(AF_INET6, SOCK_STREAM | SOCK_NONBLOCK, IPPROTO_TCP)) < 0) {
-        fprintf(stderr, "%s\n", strerror(errno));
+        perror("socket");
         exit(EXIT_FAILURE);
     }
 
@@ -18,24 +18,21 @@ void init_server(int port, char* addr, int cons, char* if_name) {
     server_addr.sin6_port = htons(port);
     server_addr.sin6_scope_id = (if_name == NULL) ? 0 : if_nametoindex(if_name);
     if (bind(listen_fd, (struct sockaddr*) &server_addr, sizeof(server_addr)) < 0) {
-        fprintf(stderr, "%s\n", strerror(errno));
+        perror("bind");
         exit(EXIT_FAILURE);
     }
 
     // 监听
     if (listen(listen_fd, cons) < 0) {
-        fprintf(stderr, "%s\n", strerror(errno));
+        perror("listen");
         exit(EXIT_FAILURE);
     }
 
     // 打印
     char s[64];
     inet_ntop(AF_INET6, &server_addr.sin6_addr, s, 64);
-    fprintf(stderr, 
-        "Server listening:\n \
-        \taddr: %s,\n \
-        \tport: %d,\n \
-        \tscope_id: %d\n",
+    fprintf(stdout, 
+        "Server init\n\taddr: %s\n\tport: %d\n\tscope_id: %d\n",
         s, server_addr.sin6_port, server_addr.sin6_scope_id
     );
 } 
